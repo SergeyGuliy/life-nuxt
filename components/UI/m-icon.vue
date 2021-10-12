9d71551e95af0fe7dc38be5548af1718e3843068
SergeyGuliy <sergey.guliy.92@gmail.com>
2021-10-12 14:42:22 +0300

    added svg loader, developing basic layout

---
+++ components/UI/m-icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="m-icon">
+    m-icon
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'MIcon',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  }
+})
+</script>
+
+<style lang='scss'>
+  .m-icon {
+
+  }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
   ],
 
   axios: {},
+
   svgLoader: {
     svgoConfig: {
       plugins: [
